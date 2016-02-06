af4dccc53324d828d7372962d1bb2c1e068fdb8d
Pierre Douyon <pgdouyon@alum.mit.edu>
2016-02-06 10:30:23 -0500

    [tmux] Cleanup shellcheck errors in battery-indicator script

--- config/tmux/tmux-battery-indicator.sh
+++ config/tmux/tmux-battery-indicator.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
-if [[ `uname` == 'Linux' ]]; then
-  current_charge=$(cat /proc/acpi/battery/BAT1/state | grep 'remaining capacity' | awk '{print $3}')
-  total_charge=$(cat /proc/acpi/battery/BAT1/info | grep 'last full capacity' | awk '{print $4}')
+if [[ $(uname) == 'Linux' ]]; then
+  current_charge=$(grep 'remaining capacity' /proc/acpi/battery/BAT1/state | awk '{print $3}')
+  total_charge=$(grep 'last full capacity' /proc/acpi/battery/BAT1/info | awk '{print $4}')
 else
-  battery_info=`ioreg -rc AppleSmartBattery`
-  current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
-  total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
+  battery_info=$(ioreg -rc AppleSmartBattery)
+  current_charge=$(echo "$battery_info" | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
+  total_charge=$(echo "$battery_info" | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
 fi
 
-charged_slots=$(echo "(($current_charge/$total_charge)*10)+1" | bc -l | cut -d '.' -f 1)
-if [[ $charged_slots -gt 10 ]]; then
+charged_slots=$(( (current_charge * 10)/total_charge + 1 ))
+if (( charged_slots > 10 )); then
   charged_slots=10
 fi
 
-if [[ $charged_slots -le 2 ]]; then
-    echo -n '#[fg=#811205,bold]'
-elif [[ $charged_slots -le 5 ]]; then
-    echo -n '#[fg=#cbe309,bold]'
+if (( charged_slots <= 2 )); then
+    echo -n '#[fg=red]'
+elif (( charged_slots <= 5 )); then
+    echo -n '#[fg=yellow]'
 else
-    echo -n '#[fg=#044712,bold]'
+    echo -n '#[fg=green]'
 fi
 
 echo -n 'Battery: ['
-for i in `seq 1 $charged_slots`; do echo -n "="; done
 
-if [[ $charged_slots -lt 10 ]]; then
-  for i in `seq 1 $(echo "10-$charged_slots" | bc)`; do echo -n " "; done
-fi
+empty_slots=$(( 10 - charged_slots ))
+for (( i=0; i < charged_slots; i++ )); do echo -n "="; done
+for (( i=0; i < empty_slots; i++ )); do echo -n " "; done
+
 echo -n ']'
 echo -n '#[default]'
